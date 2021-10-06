8e09c3bddc970649c1606391c2df31bc09f528d9
liamlylehr <lylehr@terpmail.umd.edu>
2021-10-06 13:21:28 -0700

    Pacman get_valid_moves() method mostly implemented

--- Projects/P2/src/PacMan.java
+++ Projects/P2/src/PacMan.java
@@ -16,7 +16,37 @@ public class PacMan{
 
 	public ArrayList<Location> get_valid_moves() {
 		
-		return null;	
+		// in dimensions of board and not a wall
+		int x = this.myLoc.x;
+		int y = this.myLoc.y;
+		ArrayList<Location> rv = new ArrayList<Location>();
+
+		if ( !myMap.getLoc(new Location(x-1,y-1)).contains(Map.Type.WALL) ) {
+			rv.add(new Location(x-1,y-1));
+		}
+		if ( !myMap.getLoc(new Location( x ,y-1)).contains(Map.Type.WALL) ) {
+			rv.add(new Location( x ,y-1));
+		}
+		if ( !myMap.getLoc(new Location(x+1,y-1)).contains(Map.Type.WALL) ) {
+			rv.add(new Location(x+1,y-1));
+		}
+		if ( !myMap.getLoc(new Location(x-1, y )).contains(Map.Type.WALL) ) {
+			rv.add(new Location(x-1, y ));
+		}
+		if ( !myMap.getLoc(new Location(x+1, y )).contains(Map.Type.WALL) ) {
+			rv.add(new Location(x+1, y ));
+		}
+		if ( !myMap.getLoc(new Location(x-1,y+1)).contains(Map.Type.WALL) ) {
+			rv.add(new Location(x-1,y+1));
+		}
+		if ( !myMap.getLoc(new Location( x ,y+1)).contains(Map.Type.WALL) ) {
+			rv.add(new Location( x ,y+1));
+		}
+		if ( !myMap.getLoc(new Location(x+1,y+1)).contains(Map.Type.WALL) ) {
+			rv.add(new Location(x+1,y+1));
+		}
+		
+		return rv;	
 	}
 
 	public boolean move() {
