009b6bbb2e9194d28c258dd33554a1576455e15a
shaikseemaa94 <seema.shaik94@gmail.com>
2021-10-06 17:29:43 -0400

    added getLoc

--- Projects/P2/tests/TestMapGetLoc.java
+++ Projects/P2/tests/TestMapGetLoc.java
@@ -6,6 +6,12 @@ import java.io.*;
 public class TestMapGetLoc {
 	
 	public void testMapGetLoc() {
-		return null;
+		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+		Ghost ghost = frame.addGhost(new Location(x, y), "name", Color.red); //Creates a red ghost named "name" at location x,y
+		frame.initPlayers(); //Creates all of the players
+		frame.startGame();
+
+
+
 	}
 }
