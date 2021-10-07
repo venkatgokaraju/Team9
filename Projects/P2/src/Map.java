a4b3d77114f14102a9ecc966df5865e583f9ccb2
shaikseemaa94 <seema.shaik94@gmail.com>
2021-10-07 02:50:39 -0400

    finished

--- Projects/P2/src/Map.java
+++ Projects/P2/src/Map.java
@@ -60,7 +60,7 @@ public class Map{
 	
 	public HashSet<Type> getLoc(Location loc) {
 		//wallSet and emptySet will help you write this method
-		return null;
+		return null; 
 	}
 
 	public boolean attack(String Name) {
