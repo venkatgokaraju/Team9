753246115e300e9d92da005a524e9cd50ab79c14
gnewbold-44 <gnewbol1@terpmail.umd.edu>
2021-10-06 22:08:15 -0400

    fixes

--- Projects/P2/tests/TestPacManInRange.java
+++ Projects/P2/tests/TestPacManInRange.java
@@ -2,6 +2,7 @@ import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
 
+//make more out of dimensions tests then you are done 
 public class TestPacManInRange extends TestCase {
 	boolean result1;
 	boolean result2;
@@ -16,6 +17,9 @@ public class TestPacManInRange extends TestCase {
 	boolean result11;
 	boolean result12;
 	boolean result13;
+	boolean result14;
+	boolean result15;
+	boolean result16;
 	protected void setUp() throws Exception{
 		result1 = test1Setup();
 		result2 = test2Setup();
@@ -30,6 +34,9 @@ public class TestPacManInRange extends TestCase {
 		result11 = test11Setup();
 		result12 = test12Setup();
 		result13 = test13Setup();
+		result14 = test14Setup();
+		result15 = test15Setup();
+		result16 = test16Setup();
 	}
 
 	public static boolean test1Setup() throws Exception{
@@ -45,8 +52,8 @@ public class TestPacManInRange extends TestCase {
 		frame1.addGhost(g3, "pinky", Color.red);
 		frame1.addGhost(g4, "inky", Color.red);
 		Map map1 = frame1.getMap();
-		PacMan pac1 = new PacMan("name", p1, map1);
-		return  pac1.is_ghost_in_range();
+		Ghost ghost1 = new Ghost("clyde", g1, map1);
+		return  ghost1.is_pacman_in_range();
 	}
 
 	public static boolean test2Setup() throws Exception{
@@ -62,28 +69,28 @@ public class TestPacManInRange extends TestCase {
 		frame1.addGhost(g3, "pinky", Color.red);
 		frame1.addGhost(g4, "inky", Color.red);
 		Map map1 = frame1.getMap();
-		PacMan pac1 = new PacMan("name", p1, map1);
-		return pac1.is_ghost_in_range();
+		Ghost ghost1 = new Ghost("clyde", g1, map1);
+		return  ghost1.is_pacman_in_range();
 	}
 	public static boolean test3Setup() throws Exception{
 		NoFrame frame1 = new NoFrame();
 		Location p1 = new Location(10,10);
 		Location g1 = new Location(9,9);
 		frame1.addPacMan(p1);
-		frame1.addGhost(g1, "clyde", Color.red);
+		frame1.addGhost(g1, "blinky", Color.red);
 		Map map1 = frame1.getMap();
-		PacMan pac1 = new PacMan("name", p1, map1);
-		return pac1.is_ghost_in_range();
+		Ghost ghost1 = new Ghost("blinky", g1, map1);
+		return  ghost1.is_pacman_in_range();
 	}
 	public static boolean test4Setup() throws Exception{
 		NoFrame frame1 = new NoFrame();
 		Location p1 = new Location(10,10);
 		Location g1 = new Location(9,10);
 		frame1.addPacMan(p1);
-		frame1.addGhost(g1, "clyde", Color.red);
+		frame1.addGhost(g1, "inky", Color.red);
 		Map map1 = frame1.getMap();
-		PacMan pac1 = new PacMan("name", p1, map1);
-		return pac1.is_ghost_in_range();
+		Ghost ghost1 = new Ghost("inky", g1, map1);
+		return  ghost1.is_pacman_in_range();
 	}
 	public static boolean test5Setup() throws Exception{
 		NoFrame frame1 = new NoFrame();
@@ -92,8 +99,8 @@ public class TestPacManInRange extends TestCase {
 		frame1.addPacMan(p1);
 		frame1.addGhost(g1, "clyde", Color.red);
 		Map map1 = frame1.getMap();
-		PacMan pac1 = new PacMan("name", p1, map1);
-		return pac1.is_ghost_in_range();
+		Ghost ghost1 = new Ghost("clyde", g1, map1);
+		return  ghost1.is_pacman_in_range();
 	}
 	public static boolean test6Setup() throws Exception{
 		NoFrame frame1 = new NoFrame();
@@ -102,8 +109,8 @@ public class TestPacManInRange extends TestCase {
 		frame1.addPacMan(p1);
 		frame1.addGhost(g1, "clyde", Color.red);
 		Map map1 = frame1.getMap();
-		PacMan pac1 = new PacMan("name", p1, map1);
-		return pac1.is_ghost_in_range();
+		Ghost ghost1 = new Ghost("clyde", g1, map1);
+		return  ghost1.is_pacman_in_range();
 	}
 	public static boolean test7Setup() throws Exception{
 		NoFrame frame1 = new NoFrame();
@@ -112,18 +119,18 @@ public class TestPacManInRange extends TestCase {
 		frame1.addPacMan(p1);
 		frame1.addGhost(g1, "clyde", Color.red);
 		Map map1 = frame1.getMap();
-		PacMan pac1 = new PacMan("name", p1, map1);
-		return pac1.is_ghost_in_range();
+		Ghost ghost1 = new Ghost("clyde", g1, map1);
+		return  ghost1.is_pacman_in_range();
 	}
 	public static boolean test8Setup() throws Exception{
 		NoFrame frame1 = new NoFrame();
 		Location p1 = new Location(10,10);
 		Location g1 = new Location(10,11);
 		frame1.addPacMan(p1);
-		frame1.addGhost(g1, "clyde", Color.red);
+		frame1.addGhost(g1, "pinky", Color.red);
 		Map map1 = frame1.getMap();
-		PacMan pac1 = new PacMan("name", p1, map1);
-		return pac1.is_ghost_in_range();
+		Ghost ghost1 = new Ghost("pinky", g1, map1);
+		return  ghost1.is_pacman_in_range();
 	}
 	public static boolean test9Setup() throws Exception{
 		NoFrame frame1 = new NoFrame();
@@ -132,8 +139,8 @@ public class TestPacManInRange extends TestCase {
 		frame1.addPacMan(p1);
 		frame1.addGhost(g1, "clyde", Color.red);
 		Map map1 = frame1.getMap();
-		PacMan pac1 = new PacMan("name", p1, map1);
-		return pac1.is_ghost_in_range();
+		Ghost ghost1 = new Ghost("clyde", g1, map1);
+		return  ghost1.is_pacman_in_range();
 	}
 	public static boolean test10Setup() throws Exception{
 		NoFrame frame1 = new NoFrame();
@@ -142,8 +149,8 @@ public class TestPacManInRange extends TestCase {
 		frame1.addPacMan(p1);
 		frame1.addGhost(g1, "clyde", Color.red);
 		Map map1 = frame1.getMap();
-		PacMan pac1 = new PacMan("name", p1, map1);
-		return pac1.is_ghost_in_range();
+		Ghost ghost1 = new Ghost("clyde", g1, map1);
+		return  ghost1.is_pacman_in_range();
 	}
 	public static boolean test11Setup() throws Exception{
 		NoFrame frame1 = new NoFrame();
@@ -152,8 +159,8 @@ public class TestPacManInRange extends TestCase {
 		frame1.addPacMan(p1);
 		frame1.addGhost(g1, "clyde", Color.red);
 		Map map1 = frame1.getMap();
-		PacMan pac1 = new PacMan("name", p1, map1);
-		return pac1.is_ghost_in_range();
+		Ghost ghost1 = new Ghost("clyde", g1, map1);
+		return  ghost1.is_pacman_in_range();
 	}
 	public static boolean test12Setup() throws Exception{
 		NoFrame frame1 = new NoFrame();
@@ -162,8 +169,8 @@ public class TestPacManInRange extends TestCase {
 		frame1.addPacMan(p1);
 		frame1.addGhost(g1, "clyde", Color.red);
 		Map map1 = frame1.getMap();
-		PacMan pac1 = new PacMan("name", p1, map1);
-		return pac1.is_ghost_in_range();
+		Ghost ghost1 = new Ghost("clyde", g1, map1);
+		return  ghost1.is_pacman_in_range();
 	}
 	public static boolean test13Setup() throws Exception{
 		NoFrame frame1 = new NoFrame();
@@ -172,10 +179,58 @@ public class TestPacManInRange extends TestCase {
 		frame1.addPacMan(p1);
 		frame1.addGhost(g1, "clyde", Color.red);
 		Map map1 = frame1.getMap();
-		PacMan pac1 = new PacMan("name", p1, map1);
-		return pac1.is_ghost_in_range();
+		Ghost ghost1 = new Ghost("clyde", g1, map1);
+		return  ghost1.is_pacman_in_range();
 	}
-
+	public static boolean test14Setup() throws Exception{
+		NoFrame frame1 = new NoFrame();
+		Location p1 = new Location(10,10);
+		Location g1 = new Location(11,11);
+		Location g2 = new Location(12,12);
+		Location g3 = new Location(14,14);
+		Location g4 = new Location(13,13);
+		frame1.addPacMan(p1);
+		frame1.addGhost(g1, "clyde", Color.red);
+		frame1.addGhost(g2, "blinky", Color.red);
+		frame1.addGhost(g3, "pinky", Color.red);
+		frame1.addGhost(g4, "inky", Color.red);
+		Map map1 = frame1.getMap();
+		Ghost ghost1 = new Ghost("blinky", g2, map1);
+		return  ghost1.is_pacman_in_range();
+	}
+	public static boolean test15Setup() throws Exception{
+		NoFrame frame1 = new NoFrame();
+		Location p1 = new Location(10,10);
+		Location g1 = new Location(11,11);
+		Location g2 = new Location(12,12);
+		Location g3 = new Location(14,14);
+		Location g4 = new Location(13,13);
+		frame1.addPacMan(p1);
+		frame1.addGhost(g1, "clyde", Color.red);
+		frame1.addGhost(g2, "blinky", Color.red);
+		frame1.addGhost(g3, "pinky", Color.red);
+		frame1.addGhost(g4, "inky", Color.red);
+		Map map1 = frame1.getMap();
+		Ghost ghost1 = new Ghost("pinky", g3, map1);
+		return  ghost1.is_pacman_in_range();
+	}
+	public static boolean test16Setup() throws Exception{
+		NoFrame frame1 = new NoFrame();
+		Location p1 = new Location(10,10);
+		Location g1 = new Location(11,11);
+		Location g2 = new Location(12,12);
+		Location g3 = new Location(14,14);
+		Location g4 = new Location(13,13);
+		frame1.addPacMan(p1);
+		frame1.addGhost(g1, "clyde", Color.red);
+		frame1.addGhost(g2, "blinky", Color.red);
+		frame1.addGhost(g3, "pinky", Color.red);
+		frame1.addGhost(g4, "inky", Color.red);
+		Map map1 = frame1.getMap();
+		Ghost ghost1 = new Ghost("inky", g4, map1);
+		return  ghost1.is_pacman_in_range();
+	}
+	
 	public void testPacManInRange1() throws FileNotFoundException{
 		assertTrue(result1 == true);
 	}
@@ -215,6 +270,15 @@ public class TestPacManInRange extends TestCase {
 	public void testPacManInRange13() throws FileNotFoundException{
 		assertTrue(result13 == false);
 	}
+	public void testPacManInRange14() throws FileNotFoundException{
+		assertTrue(result14 == false);
+	}
+	public void testPacManInRange15() throws FileNotFoundException{
+		assertTrue(result15 == false);
+	}
+	public void testPacManInRange16() throws FileNotFoundException{
+		assertTrue(result16 == false);
+	}
+	
 }
-
 
