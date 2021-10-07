d83f265202da2e5b2c2ad7e27ce8022c2d30aacd
shaikseemaa94 <seema.shaik94@gmail.com>
2021-10-06 22:36:39 -0400

    added one test

--- Projects/P2/tests/TestAttack.java
+++ Projects/P2/tests/TestAttack.java
@@ -3,8 +3,29 @@ import java.awt.Color;
 import java.io.*;
 
 public class TestAttack extends TestCase {
+	boolean result;
+
+	protected void setUp() throws Exception{
+		result = test1Setup();
+	}
+
+	public static boolean test1Setup() throws Exception{
+		NoFrame frame1 = new NoFrame();
+		Location p1 = new Location(10,10);
+		Location g1 = new Location(11,11);
+
+		frame1.addPacMan(p1);
+		frame1.addGhost(g1,"clyde", Color.red);
+
+		Map map1 = frame1.getMap();
+
+		PacMan pac1 = new PacMan("name", p1, map1);
+		Ghost ghost1 = new Ghost("clyde", g1, map1);
+		
+		return ghost1.attack();
+	}
 
 	public void testAttack() throws FileNotFoundException{
-		return null;	
+		assertTrue(result == true); 
 	}
 }
