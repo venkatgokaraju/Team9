15ea41d281849c328665dfa7c848a000abad1784
Kwesi <kwesi.b.owubah@gmail.com>
2021-10-07 16:02:47 -0400

    testing FN PM Move

--- Projects/P2/src/PacMan.java
+++ Projects/P2/src/PacMan.java
@@ -15,8 +15,39 @@ public class PacMan{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
+		// Liam Code
+		int x = this.myLoc.x;
+		int y = this.myLoc.y;
+		ArrayList<Location> rv = new ArrayList<Location>();
+
+		if ( !myMap.getLoc(new Location(x-1,y-1)).contains(Map.Type.WALL) ) {
+			rv.add(new Location(x-1,y-1));
+		}
+		if ( !myMap.getLoc(new Location( x ,y-1)).contains(Map.Type.WALL) ) {
+			rv.add(new Location( x ,y-1));
+		}
+		if ( !myMap.getLoc(new Location(x+1,y-1)).contains(Map.Type.WALL) ) {
+			rv.add(new Location(x+1,y-1)); 
+		}
+		if ( !myMap.getLoc(new Location(x-1, y )).contains(Map.Type.WALL) ) {
+			rv.add(new Location(x-1, y ));
+		}
+		if ( !myMap.getLoc(new Location(x+1, y )).contains(Map.Type.WALL) ) {
+			rv.add(new Location(x+1, y ));
+		}
+		if ( !myMap.getLoc(new Location(x-1,y+1)).contains(Map.Type.WALL) ) {
+			rv.add(new Location(x-1,y+1));
+		}
+		if ( !myMap.getLoc(new Location( x ,y+1)).contains(Map.Type.WALL) ) {
+			rv.add(new Location( x ,y+1));
+		}
+		if ( !myMap.getLoc(new Location(x+1,y+1)).contains(Map.Type.WALL) ) {
+			rv.add(new Location(x+1,y+1));
+		}
+
+		return rv;	
 		
-		return null;	
+//		return null;	
 	}
 
 	/*TODO: check if we need to create getters for Location.java (x & y)*/
--- Projects/P2/tests/TestPacManMove.java
+++ Projects/P2/tests/TestPacManMove.java
@@ -1,10 +1,33 @@
 import junit.framework.*;
+
+//import static org.junit.Assert.assertEquals;
+
 import java.awt.Color;
 import java.io.*;
 
 public class TestPacManMove extends TestCase {
+//public class TestPacManMove {
 
-	public void testPacManMove() throws FileNotFoundException{
-		return null;
+	
+	public void init() throws FileNotFoundException {
+		NoFrame frame = new NoFrame();
+
+//		MainFrame frame = new MainFrame();
 	}
+
+	public void testPacManMove() throws FileNotFoundException {
+		int i = 0;
+		NoFrame frame = new NoFrame();
+		Location locationPacman = new Location(10, 10);
+		
+		frame.addPacMan(locationPacman);
+		Map grid = frame.getMap();
+		PacMan pacman = new PacMan("PM", locationPacman, grid);
+		
+//		assertTrue(pacman.move());
+		
+		assertEquals(i, 0);
+		
+	}
+	
 }
