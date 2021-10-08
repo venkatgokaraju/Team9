a4baae117badce16427d98d0319c9b118bc5eb5a
shaikseemaa94 <seema.shaik94@gmail.com>
2021-10-07 20:08:17 -0400

    fixed tests

--- Projects/P2/tests/TestConsume.java
+++ Projects/P2/tests/TestConsume.java
@@ -5,6 +5,19 @@ import java.io.*;
 public class TestConsume extends TestCase {
 
 	public void testConsume() throws FileNotFoundException {
-		return null;	
+		NoFrame frame1 = new NoFrame();
+		Location c1 = new Location(1,5); // cookie and pacman location here 
+
+		frame1.addPacMan(c1);
+
+		Map map1 = frame1.getMap();
+		PacMan pacman1 = new PacMan("boo", c1, map1);
+
+		System.out.println(map1.getLoc(c1)); // [COOKIE, PACMAN]
+
+		System.out.println(pacman1.consume()); // returns null 
+
+		assertNotNull(pacman1.consume());
+
 	}
 }
